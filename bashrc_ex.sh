0478fc49770d0fbae49770b354060ddecb4b9998
GNagahashi <88316240+GNagahashi@users.noreply.github.com>
2023-03-14 23:47:45 +0900

    add

---
+++ bashrc_ex.sh
@@ -0,0 +1,7 @@
+# extra bash setting
+
+# change prompt
+export PS1='\[\e]0;\h@\u\a\]\n\[\e[38;2;0;255;162m\]\u: \w\n\$ \[\e[0m\]'
+
+# disable(overwriting): alias ls='ls --color=auto'
+alias ls='ls'
