6c536b050550de40eb2e40d662ab196ea473d2c5
GNagahashi <88316240+GNagahashi@users.noreply.github.com>
2023-03-15 16:59:11 +0900

    add program

---
+++ programs/get_key.py
@@ -0,0 +1,29 @@
+import select
+import sys
+import termios
+import tty
+
+
+def get_key():
+    """Get the first character input from the keyboard."""
+    settings = termios.tcgetattr(sys.stdin)
+    tty.setraw(sys.stdin.fileno())
+
+    rlist, _, _ = select.select([sys.stdin], [], [])
+    if rlist:
+        key = sys.stdin.read(1)
+    else:
+        key = ''
+
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+    return key
+
+
+def main():
+    for i in range(10):
+        key = get_key()
+        print('input: "{}"'.format(key))
+
+
+if __name__ == '__main__':
+    main()
