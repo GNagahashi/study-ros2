e1c95771f020365c66a53475d91c0bf4dac63dce
GNagahashi <88316240+GNagahashi@users.noreply.github.com>
2023-03-17 14:03:13 +0900

    edit

--- programs/teleop_burger.py
+++ programs/teleop_burger.py
@@ -17,6 +17,9 @@ class TeleopBurger(Node):
     BURGER_MAX_ANG_VEL = 2.84
     BURGER_MAX_LIN_VEL = 0.22
 
+    ANG_VEL_STEP_SIZE = 0.1
+    LIN_VEL_STEP_SIZE = 0.01
+
     def __init__(self):
         """Telemetry operation of TurtleBot3(burger) with a keyboard"""
         super().__init__('teleop_burger')
@@ -44,7 +47,7 @@ class TeleopBurger(Node):
         """Verify that the linear velocity does not exceed the threshold."""
         return self.__constrain(velocity, -self.BURGER_MAX_LIN_VEL, self.BURGER_MAX_LIN_VEL)
 
-    def current_velocity(self):
+    def get_current_velocity(self):
         """Return current velocity: (angular, linear)"""
         angular = [self.__twist.angular.x, self.__twist.angular.y, self.__twist.angular.z]
         linear = [self.__twist.linear.x, self.__twist.linear.y, self.__twist.linear.z]
@@ -56,7 +59,7 @@ class TeleopBurger(Node):
         settings = termios.tcgetattr(sys.stdin)
         tty.setcbreak(sys.stdin)
 
-        rlist, _, _ = select.select([sys.stdin], [], [])
+        rlist, _, _ = select.select([sys.stdin], [], [], 0.1)
         if rlist:
             key = sys.stdin.read(1)
         else:
@@ -92,23 +95,62 @@ def main():
 
     print('start')
 
-    key = teleop_burger.get_key()
-    if key == 'w':
-        # @todo わざわざチェックとセットを別にする必要はないのでは？(set_..._velocity()の中でcheck_...をする)
-        target_linear_velocity = teleop_burger.check_linear_limit_velocity(0.2)
-    elif key == 's':
-        target_linear_velocity = 0.0
+    target_lin_vel_x = 0.0
+    target_ang_vel_z = 0.0
 
-    teleop_burger.set_linear_velocity([target_linear_velocity, 0.0, 0.0])
-    teleop_burger.publish_velocity()
+    try:
+        while(1):
+            current_ang_vel, current_lin_vel = teleop_burger.get_current_velocity()
 
-    # @todo set_...の引数がリストであることをわかりやすくしたい
-    teleop_burger.set_angular_velocity([0.0, 0.0, 0.0])
-    teleop_burger.set_linear_velocity([0.0, 0.0, 0.0])
-    teleop_burger.publish_velocity()
+            key = teleop_burger.get_key()
+
+            if key == 'w':
+                target_ang_vel_z = current_ang_vel[2]
+                target_lin_vel_x = teleop_burger.check_linear_limit_velocity(current_lin_vel[0] + teleop_burger.LIN_VEL_STEP_SIZE)
+            elif key == 'a':
+                target_ang_vel_z = teleop_burger.check_angular_limit_velocity(current_ang_vel[2] + teleop_burger.ANG_VEL_STEP_SIZE)
+                target_lin_vel_x = current_lin_vel[0]
+            elif key == 's':
+                target_ang_vel_z = current_ang_vel[2]
+                target_lin_vel_x = teleop_burger.check_linear_limit_velocity(current_lin_vel[0] - teleop_burger.LIN_VEL_STEP_SIZE)
+            elif key == 'd':
+                target_ang_vel_z = teleop_burger.check_angular_limit_velocity(current_ang_vel[2] - teleop_burger.ANG_VEL_STEP_SIZE)
+                target_lin_vel_x = current_lin_vel[0]
+            elif key == ' ':
+                target_ang_vel_z = 0.0
+                target_lin_vel_x = 0.0
+            else:
+                target_ang_vel_z = current_ang_vel[2]
+                target_lin_vel_x = current_lin_vel[0]
+
+            teleop_burger.set_angular_velocity([0.0, 0.0, target_ang_vel_z])
+            teleop_burger.set_linear_velocity([target_lin_vel_x, 0.0, 0.0])
+
+            teleop_burger.publish_velocity()
+    finally:
+        teleop_burger.set_angular_velocity([0.0, 0.0, 0.0])
+        teleop_burger.set_linear_velocity([0.0, 0.0, 0.0])
+        teleop_burger.publish_velocity()
 
     print('end')
-    pass
+
+    # key = teleop_burger.get_key()
+    # if key == 'w':
+    #     # @todo わざわざチェックとセットを別にする必要はないのでは？(set_..._velocity()の中でcheck_...をする)
+    #     target_linear_velocity = teleop_burger.check_linear_limit_velocity(0.2)
+    # elif key == 's':
+    #     target_linear_velocity = 0.0
+
+    # teleop_burger.set_linear_velocity([target_linear_velocity, 0.0, 0.0])
+    # teleop_burger.publish_velocity()
+
+    # # @todo set_...の引数がリストであることをわかりやすくしたい
+    # teleop_burger.set_angular_velocity([0.0, 0.0, 0.0])
+    # teleop_burger.set_linear_velocity([0.0, 0.0, 0.0])
+    # teleop_burger.publish_velocity()
+
+    # print('end')
+    # pass
 
 
 if __name__ == '__main__':
